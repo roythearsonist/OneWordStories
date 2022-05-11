01c60d7ad3206a5aa7f14f2d8f936947c8cb1af6
CharlieS1103 <someonelikeswindows@gmail.com>
2022-05-11 15:45:56 -0400

    Add styling to card

--- one-word-stories/src/Components/Card.jsx
+++ one-word-stories/src/Components/Card.jsx
@@ -1,5 +1,6 @@
 
 import {Modal} from '../Components'
+import './ComponentCSS/card.css';
 function Card(props){
 
     return (
@@ -7,6 +8,7 @@ function Card(props){
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.text}</p>
+                <button className="popup-modal-button">Add to Story</button> 
                 <Modal text={props.text}></Modal>
             </div>
         </div>
