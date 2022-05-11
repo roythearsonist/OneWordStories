dff71e454bdab2f809a638baa9a837a8b7d112f9
CharlieS1103 <someonelikeswindows@gmail.com>
2022-05-10 22:08:31 -0400

    Hotfix to the Tauri libs
    I totally wasn't the one who they had to make the hotfix for ;)

--- one-word-stories/src/Components/Navigation.jsx
+++ one-word-stories/src/Components/Navigation.jsx
@@ -5,7 +5,7 @@
         <div class="topnav">
           <img src={logo} alt="logo" class="logo" />
           <a class="active" href="/">Home</a>
-        <a href="https://github.com/CharlieS1103/OneWordStories">Github</a>
+        <a href="https://github.com/CharlieS1103/OneWordStories" rel="noreferrer" target="_blank">Github</a>
           <a href="contact">Contact</a>
           <a href="about">About</a>
         </div>
